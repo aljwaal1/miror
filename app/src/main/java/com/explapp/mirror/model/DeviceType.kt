b60d287108bcb4a7e023e7a245cc6f868d425f40
aljwaal1 <yaya15112016@gmail.com>
2026-07-09 23:50:06 +0300

    Add device type model

---
+++ app/src/main/java/com/explapp/mirror/model/DeviceType.kt
@@ -0,0 +1,15 @@
+package com.explapp.mirror.model
+
+enum class DeviceType(val arabicName: String) {
+    TV("شاشة"),
+    ANDROID_TV("Android TV"),
+    CHROMECAST("Chromecast"),
+    DLNA("DLNA"),
+    UPNP("UPnP"),
+    ANYVIEW("Anyview Cast"),
+    AIRPLAY("AirPlay"),
+    PHONE("هاتف"),
+    COMPUTER("كمبيوتر"),
+    ROUTER("راوتر"),
+    UNKNOWN("غير معروف")
+}
