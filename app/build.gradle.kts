e21f0b1d18d261ae13fe1b3f6bb929d2375ee6d4
aljwaal1 <yaya15112016@gmail.com>
2026-07-10 05:33:11 +0300

    Add coroutine dependency for discovery and media sending

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,4 +41,5 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
 }
